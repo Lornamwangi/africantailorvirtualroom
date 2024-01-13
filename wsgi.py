307981fae206d81f83a03239bd409aee75f545af
Lornamwangi <149935850+Lornamwangi@users.noreply.github.com>
2024-01-13 19:55:10 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from Africantailorvirtual3D import app  # Replace 'Africantailorvirtual3Ddressingroom' with your actual Flask app filename
+from africantailorvirtual import app  # Replace 'Africantailorvirtual3Ddressingroom' with your actual Flask app filename
 
 if __name__ == "__main__":
     app.run(app.py)
